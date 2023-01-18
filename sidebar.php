745d8021e1b947a6c292ca10e274e8e0928d94d8
Habibur Rahman <41167924+habibur899@users.noreply.github.com>
2023-01-19 01:02:06 +0600

    category

--- sidebar.php
+++ sidebar.php
@@ -7,16 +7,7 @@
         ?>
 
 
-        <div class="sidebar-newsletter centred">
-            <div class="title"><i class="fa fa-envelope-o"></i>&nbsp;&nbsp;NEWSLETTER</div>
-            <div class="text">These days are all share them wit me</div>
-            <form action="#" method="post">
-                <div class="form-group">
-                    <input type="email" name="email" placeholder="Your Email Address" required="">
-                    <button type="submit" class="btn-one">SUBSCRIBE</button>
-                </div>
-            </form>
-        </div>
+
         <div class="sidebar-instagram">
             <div class="sidebar-title">INSTAGRAM</div>
             <ul class="img-list clearfix">
@@ -50,16 +41,6 @@
                                     src="<?php echo get_template_directory_uri() ?>/images/home/i6.jpg"
                                     alt=""></a></figure>
                 </li>
-            </ul>
-        </div>
-        <div class="sidebar-categories">
-            <div class="sidebar-title">CATEGORIES</div>
-            <ul class="categories-list">
-                <li><a href="#">Beauty<span>(3)</span></a></li>
-                <li><a href="#">Dressing<span>(8)</span></a></li>
-                <li><a href="#">Fitness <span>(7)</span></a></li>
-                <li><a href="#">Lifestyle<span>(6)</span></a></li>
-                <li><a href="#">Travel<span>(9)</span></a></li>
             </ul>
         </div>
         <div class="sidebar-img-content">
